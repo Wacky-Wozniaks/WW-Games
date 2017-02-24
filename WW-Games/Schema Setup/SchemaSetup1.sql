5c6fa2cb6d53afc8953dd8b1663460cefd16d5b9
juliamcclellan <jmcclellan@mxschool.edu>
2017-02-24 13:44:03 -0500

    Create user table

---
+++ WW-Games/Schema Setup/SchemaSetup1.sql
@@ -0,0 +1,13 @@
+CREATE TABLE public.users
+(
+    id integer NOT NULL,
+    email text COLLATE pg_catalog."default" NOT NULL,
+    password text COLLATE pg_catalog."default" NOT NULL,
+    first_name text COLLATE pg_catalog."default" NOT NULL,
+    last_name text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT users_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
