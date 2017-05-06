474281f260325636a8ca80f18dbe53876b2eccfd
juliamcclellan <jmcclellan@mxschool.edu>
2017-05-06 08:29:32 -0400

    Second minimax difficulty level

--- WW-Games-2/src/main/java/com/wackywozniaks/games/connect/ConnectAI.java
+++ WW-Games-2/src/main/java/com/wackywozniaks/games/connect/ConnectAI.java
@@ -7,16 +7,17 @@ import com.wackywozniaks.games.PlayerAI;
  * An AI for Connect games
  * 
  * @author WackyWozniaks Company
- * @version 04/29/2017
+ * @version 05/06/2017
  */
 public class ConnectAI extends PlayerAI
 {
-	public static final int RANDOM = 0, MINIMAX = 1;
+	public static final int RANDOM = 0, MINIMAX = 1, MINIMAX2 = 2;
 	
 	public static ConnectMove chooseMove(Game g, int difficulty)
 	{
 		if(difficulty == RANDOM) return (ConnectMove)random(g);
-		else if(difficulty == MINIMAX) return (ConnectMove)minimax(g, 3);
+		else if(difficulty == MINIMAX) return (ConnectMove)minimax(g, 2);
+		else if(difficulty == MINIMAX2) return (ConnectMove)minimax(g, 4);
 		else return null;
 	}
 }
