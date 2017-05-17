b5de55dd63f5f70d50c13997ae6b33db300b6590
juliamcclellan <jmcclellan@mxschool.edu>
2017-05-17 10:21:37 -0400

    Different checkers AI levels

--- WW-Games-2/src/main/java/com/wackywozniaks/games/checkers/CheckersAI.java
+++ WW-Games-2/src/main/java/com/wackywozniaks/games/checkers/CheckersAI.java
@@ -7,18 +7,18 @@ import com.wackywozniaks.games.PlayerAI;
  * The AI for Checkers
  * 
  * @author WackyWozniaks Company
- * @version 05/13/2017
+ * @version 05/17/2017
  */
 public class CheckersAI extends PlayerAI {
 	
-	public static final int RANDOM = 0;
-	public static final int MINIMAX = 1;
-	public static final int MINIMAX2 = 2;
+	public static final int MINIMAX1 = 0;
+	public static final int MINIMAX2 = 1;
+	public static final int MINIMAX3 = 2;
 	
 	public static CheckersMove chooseMove(Game g, int difficulty){
-		if(difficulty == RANDOM) return (CheckersMove)random(g);
-		else if(difficulty == MINIMAX) return (CheckersMove)minimax(g, 2);
-		else if(difficulty == MINIMAX2) return (CheckersMove)minimax(g, 4);
+		if(difficulty == MINIMAX1) return (CheckersMove)minimax(g, 2);
+		else if(difficulty == MINIMAX2) return (CheckersMove)minimax(g, 5);
+		else if(difficulty == MINIMAX3) return (CheckersMove)minimax(g, 8);
 		else return null;
 	}
 
